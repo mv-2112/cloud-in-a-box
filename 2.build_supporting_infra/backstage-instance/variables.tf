cff9d8ad8125ace95b45ba5ef025524bc24ad26c
Matt Verran <verranm76@gmail.com>
2023-10-09 15:47:21 +0100

    tidy up

--- 2.build_supporting_infra/backstage-instance/variables.tf
+++ 2.build_supporting_infra/backstage-instance/variables.tf
@@ -1,10 +1,10 @@
-variable "project" { default = "playground" }
-variable "image" { default = "Debian-10" }
+variable "project" { default = "builder" }
+variable "image" { default = "ubuntu" }
 variable "flavour" { default = "m1.medium" }
 variable "externalGateway" { default = "external-network" }
 variable "tcp_ports" { default = [ 22,3000, 7007 ]}
 variable "domain" {
   type = string
-  default = "local"
+  default = "example.com"
 }
 variable "expose" { default = false }
