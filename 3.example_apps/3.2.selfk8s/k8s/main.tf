278aa245015cfeee02e1ffbbaa842bd936cca77f
Matt Verran <verranm76@gmail.com>
2023-12-13 19:23:29 +0000

    remove self build k8s

--- 3.example_apps/3.2.selfk8s/k8s/main.tf
+++
@@ -1,7 +0,0 @@
-data "openstack_networking_network_v2" "external_network" {
-  name = "ext_net"
-}
-
-data "openstack_dns_zone_v2" "this-domain" {
-  name        = "${var.project}.${var.domain}."
-}
--- 3.example_apps/3.2.selfk8s/k8s/provider.tf
+++
@@ -1,8 +0,0 @@
-# Define required providers
-terraform {
-  required_providers {
-    openstack = {
-      source  = "terraform-provider-openstack/openstack"
-    }
-  }
-}
