6d80c45d0db264015b148696fabbed341b9ecb4b
mv-2112 <verranm76@gmail.com>
2024-07-30 11:14:13 +0100

    feat: add buckets back in

---
+++ 1.start_here/resource-bucket.tf
@@ -0,0 +1,34 @@
+resource "openstack_objectstorage_container_v1" "build_container" {
+  region = "RegionOne"
+  name   = "${var.build_project}-bucket-1"
+
+  #   versioning_legacy {
+  #     type     = "versions"
+  #     location = "tf-test-container-versions"
+  #   }
+
+  versioning = true
+
+  container_read  = ".r:-${openstack_identity_user_v3.domain_user_1.name}"
+  container_write = "${openstack_identity_user_v3.domain_user_1.name}"
+}
+
+
+
+resource "openstack_objectstorage_object_v1" "build_doc" {
+  region         = "RegionOne"
+  container_name = openstack_objectstorage_container_v1.build_container.name
+  name           = "test/default.json"
+
+  content_type = "application/json"
+  source       = "./default.json"
+}
+
+resource "openstack_objectstorage_container_v1" "app_container" {
+  for_each = toset(var.app_projects)
+  region   = "RegionOne"
+  name     = "${each.key}-bucket-1"
+}
+
+
+
