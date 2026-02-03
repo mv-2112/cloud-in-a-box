1f2b52dbd83bead5bcf7caf5152c0abcdf960a54
Matt Verran <verranm76@gmail.com>
2026-02-03 19:28:37 +0000

    update harness 0.2.3

--- start_here/modules/harness/main.tf
+++ start_here/modules/harness/main.tf
@@ -1,13 +1,13 @@
 module "delegate" {
   source = "harness/harness-delegate/kubernetes"
-  version = "0.2.0"
+  version = "0.2.3"
   account_id = var.account_id
   delegate_token = var.delegate_token
   delegate_name = "terraform-delegate"
   deploy_mode = "KUBERNETES"
   namespace = "harness-delegate-ng"
   manager_endpoint = "https://app.harness.io"
-  delegate_image = "harness/delegate:25.03.85403"
+  delegate_image = "us-docker.pkg.dev/gar-prod-setup/harness-public/harness/delegate:26.01.88303"
   replicas = 1
   upgrader_enabled = true
 }
